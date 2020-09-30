b3b40d0a6b03e4eaadf42075590b2e682f17c08a
Benjamin Hills <bhills@uw.edu>
2020-09-30 16:34:43 -0600

    Add rotational transform from Young et al. (2020)

--- quad_pol_processing.py
+++ quad_pol_processing.py
@@ -8,65 +8,118 @@ Created on Thu Aug 13 2020
 
 import numpy as np
 
-# ------------- From Doake et al. 2003 ----------------------- #
+# ------------- From Jordan et al. 2019 ----------------------- #
 
-def P(δ):
+def P(delta):
     """
-    phase-shift matrix
-    Doake et al. (2003) pg. 6
+    propagation/transmission matrix
+    Jordan et al. (2019)
 
     Parameters
     ---------
     δ:     two-way birefringent phase shift
     """
-    return np.array([[1,0],
-                     [0,np.exp(1j*δ)]])
+    return np.array([[np.exp(1j*delta),0],
+                     [0,1.]])
 
-def S(r):
+def Gamma(r):
     """
-    target scattering matrix
-    Doake et al. (2003) pg. 6
+    Reflection matrix
+    Jordan et al. (2019)
 
     Parameters
     ---------
     r:     ratio of reflection coefficients
     """
-    return np.array([[1,0],
-                     [0,r]])
+    return np.array([[r,0],
+                     [0,1.]])
 
-def R(θ):
+def R(alpha):
     """
-    rotation matrix
-    Doake et al. (2003) pg. 6
+    2-D rotation matrix
+    Jordan et al. (2019)
 
     Parameters
     ---------
-    θ:     rotation angle
-            can be between measurement axis and optic axis (ϵ)
-            or between optic axis and reflection axis (γ)
+    alpha:     rotation angle between measurement axis and optic axis (ϵ)
     """
-    return np.array([[np.cos(θ),np.sin(θ)],
-                     [-np.sin(θ),np.cos(θ)]])
+    return np.array([[np.cos(alpha),np.sin(alpha)],
+                     [-np.sin(alpha),np.cos(alpha)]])
 
-def Sm(ϵ,δ,γ,r):
+def S(alpha,delta,r):
     """
     Sinclair scattering matrix
-    Doake et al. (2003) eq. (5)
+    Jordan et al. (2019) eq. (7)
 
     Parameters
     ---------
-    ϵ:     rotation angle between measurement axis and optic axis
-    δ:     two-way birefringent phase shift
-    γ:     rotation angle between optic axis and reflection axis
+    alpha:     rotation angle between measurement axis and optic axis
+    delta:     two-way birefringent phase shift
     r:     ratio of reflection coefficients
     """
 
-    M = np.matmul(R(ϵ),P(δ/2.))
-    M = np.matmul(M,R(γ))
-    M = np.matmul(M,S(r))
-    M = np.matmul(M,R(-γ))
-    M = np.matmul(M,P(δ/2.))
-    M = np.matmul(M,R(-ϵ))
+    M = np.matmul(R(alpha),P(delta/2.))
+    M = np.matmul(M,Gamma(r))
+    M = np.matmul(M,P(delta/2.))
+    M = np.matmul(M,np.transpose(R(alpha)))
 
     return M
 
+def rot_shift(S,theta):
+    """
+    Azimuthal (rotational) shift of principal axes
+    at the transmitting and receiving antennas
+    Mott, 2006
+
+    Parameters
+    --------
+    S : array
+        2-d array with [[shh,svh][shv,svv]] of complex numbers
+    theta : complex
+            rotational offset
+    """
+
+    shh = S[0,0]
+    svh = S[0,1]
+    shv = S[1,0]
+    svv = S[1,1]
+
+    S_ = np.empty_like(S)
+    S_[0,0] = shh*np.cos(theta)**2.+(svh+shv)*np.sin(theta)*np.cos(theta)+svv*np.sin(theta)**2
+    S_[0,1] = shv*np.cos(theta)**2.+(svv-shh)*np.sin(theta)*np.cos(theta)-svh*np.sin(theta)**2
+    S_[1,0] = svh*np.cos(theta)**2.+(svv-shh)*np.sin(theta)*np.cos(theta)-shv*np.sin(theta)**2
+    S_[1,1] = svv*np.cos(theta)**2.-(svh+shv)*np.sin(theta)*np.cos(theta)+shh*np.sin(theta)**2
+
+    return S_
+
+def phase_shift(z,freq=200e6,eps_bi=0.00354,eps=3.15,c=3e8):
+    """
+    Two-way phase shift
+    Jordan et al. (2019)
+
+    Parameters
+    ---------
+    z:
+    freq:
+    eps_bi:
+    eps:
+    c:
+
+    """
+    delta = 4.*np.pi*freq/c*(z*eps_bi/(2.*np.sqrt(eps)))
+    return delta
+
+def coherence(s1,s2):
+    """
+    phase correlation between two elements of the scattering matrix
+    Jodan et al. (2019) eq. 13
+
+    Parameters
+    ---------
+    s1:
+    s2:
+    """
+    top = np.dot(s1,np.conj(s2))
+    bottom = np.sqrt(np.abs(s1)**2.)*np.sqrt(np.abs(s2)**2.)
+    c = top/bottom
+    return c
